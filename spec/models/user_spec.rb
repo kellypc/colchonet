5d67c06c8f89b2de8ad51e16e088517fefec1ac9
Kelly Cruz <kelly.cruz@rebase.com.br>
2021-03-08 15:56:46 -0300

    Adiciona validação para usuário

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validation' do
+    it 'é valido' do
+      user = User.new( full_name: 'Mario Bros',
+                       email: 'mrio@gmail.com',
+                       location: 'São Paulo, Brasil',
+                       password: 'segredo' )
+
+     expect(user).to be_valid
+    end
+
+    it "é invalido sem o nome completo" do
+      user = User.new(full_name: nil)
+
+      user.valid?
+
+      expect(user.errors[:full_name]).to include("can't be blank")
+    end
+
+    it "é invalido sem o email" do
+      user = User.new(email: nil)
+
+      user.valid?
+
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "é invalido sem a localização" do
+      user = User.new(location: nil)
+
+      user.valid?
+
+      expect(user.errors[:location]).to include("can't be blank")
+    end
+
+    it "é invalido sem a senha" do
+      user = User.new(password: nil)
+
+      user.valid?
+
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+  end
+end
